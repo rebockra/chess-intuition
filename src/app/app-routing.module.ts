f59b66fb099363b3db27892f8137d7af67277cdd
Rene Bockrandt <rene.bockrandt@gmail.com>
2019-07-08 17:21:01 +0200

    Remove advanced routing with lazy loading of gmodu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { GameComponent } from './game/game/game.component';
 
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent},
-  { path: 'game', loadChildren: 'src/app/game/game.module#GameModule'}
+  { path: 'game', component: GameComponent}
+  //{ path: 'game', loadChildren: 'src/app/game/game.module#GameModule'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { GameComponent } from './game/game/game.component';
+import { BoardComponent } from './game/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    GameComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
